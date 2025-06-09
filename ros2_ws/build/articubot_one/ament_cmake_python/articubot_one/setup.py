c6cd570675a3220dee9dab67bb43a5b1085ac332
Ahmet monem <ahmad.y.syria@gmail.com>
2025-06-09 14:45:29 +0000

    awdawd

--- ros2_ws/build/articubot_one/ament_cmake_python/articubot_one/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='articubot_one',
-    version='0.0.0',
-    packages=find_packages(
-        include=('articubot_one', 'articubot_one.*')),
-)
